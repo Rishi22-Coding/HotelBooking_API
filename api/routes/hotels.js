8b84baed096c3db0092855b8f842b52628aebae8
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2023-04-21 20:35:34 +0530

    CRUD is workinng for Hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,6 +2,28 @@ import express from "express";
 const router = express.Router();
 import Hotel from "../models/Hotel.js";
 
+//Get All
+router.get("/", async(req, res)=>{
+  console.log(req.body);
+  try{
+    const hotels = await Hotel.find(req.params.id);
+    res.status(200).json(hotels);
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
+
+//Get One
+router.get("/:id", async(req, res)=>{
+  console.log(req.body);
+  try{
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
+
 //Create
 router.post("/", async(req, res)=>{
   const newHotel = new Hotel(req.body);
@@ -12,6 +34,28 @@ router.post("/", async(req, res)=>{
   }catch(err){
     res.status(500).json(err);
   }
-})
+});
+
+//update
+router.put("/:id", async(req, res)=>{
+  console.log(req.body);
+  try{
+    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body}, {new: true});
+    res.status(200).json(updatedHotel);
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
+
+//Delete
+router.delete("/:id", async(req, res)=>{
+  console.log(req.body);
+  try{
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been Removed!");
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
 
 export default router;
