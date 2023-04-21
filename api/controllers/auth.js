cbc23ee0757f67e56b0ce3ff33b1f9f97fdb0a80
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2023-04-21 23:00:22 +0530

    login and register is working properly

---
+++ api/controllers/auth.js
@@ -0,0 +1,39 @@
+import User from "../models/User.js"
+import bcrypt from"bcryptjs";
+import {createError} from "../utils/error.js"
+
+export const register=async(req, res, next)=>{
+  try{
+    const salt = bcrypt.genSaltSync(10);
+    const hashed = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashed
+    })
+    await newUser.save();
+    res.status(200).send("User has been Created!");
+  }catch(err){
+    next(err);
+  }
+}
+
+export const login=async(req, res, next)=>{
+  try{
+    console.log(req.body.username);
+    const user=await User.findOne({username: req.body.username});
+    console.log(user);
+    if(!user){
+      return next(createError(404, "User not Found!"));
+    }
+    const isPasswordCorrect=await bcrypt.compare(req.body.password, user.password);
+    if(!isPasswordCorrect){
+      return next(createError(400, "Wrong Password!"));
+    }
+
+    const {password, isAdmin, ...otherDetails}=user._doc;
+    res.status(200).json({...otherDetails});
+  }catch(err){
+    next(err);
+  }
+}
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,11 +1,8 @@
 import express from "express";
+import { login, register } from "../controllers/auth.js";
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-  res.send("Auth EndPoint");
-})
-router.get("/register", (req, res)=>{
-  res.send("Auth Register EndPoint");
-})
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
