1b8d123e3c26252f86b7b552afd0107f5561a9e8
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2023-04-23 10:18:18 +0530

    Rooms and Hotels relationship were made

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,21 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import {verifyAdmin} from"../utils/verifyToken.js";
 const router = express.Router();
 
+//Get All
+router.get("/", getRooms);
+
+//Get One
+router.get("/:id", getRoom);
+
+//Create
+router.post("/:hotelid", verifyAdmin , createRoom)
+
+//update
+router.put("/:id", verifyAdmin, updateRoom);
+
+//Delete
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
 
 export default router;
